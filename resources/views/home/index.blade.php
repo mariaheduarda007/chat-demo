b88a4fa69227596a4cc7866bca89e94cefdabfcf
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-11-11 22:29:56 -0300

    Front em progresso

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="{{ asset('css/login.css') }}"> -->
+    @vite('resources/css/login.css')
+
+    <title>Document</title>
+</head>
+
+<body class="screen">
+   <div class="home-container"> 
+     <div class="chats-container">
+    </div>
+    <div class="messages-container">
+       <div class="message-input-area">  <input class="input"  placeholder="Participe do chat!">
+       <div class="button-send"> Enviar </div> </div>
+       <div class="message"> OI </div>
+    </input>
+   </div>
+</body>
+
+</html>
